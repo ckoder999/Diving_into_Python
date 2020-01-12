4744e67432adc00b70b914163cc3a927911352a3
ckoder999 <ckoder999@gmail.com>
2020-01-12 20:51:04 +0500

    added new file

---
+++ 2_10_func_prog.py
@@ -0,0 +1,23 @@
+# функция, которая создает другую функцию
+def get_multiplier():
+    def inner(a, b):
+        return a * b
+    return inner
+
+
+mul = get_multiplier()
+print(mul(3, 10))
+
+#теперь это  inner!
+print(mul.__name__)
+
+# замыкание - функция inner использует переменную из области видимости функции get_multiplier_num
+def get_multiplier_num(number):
+    def inner(a):
+        return a * number
+    return inner
+
+mul1 = get_multiplier_num(3)
+print(mul1(15))
+
+#map
